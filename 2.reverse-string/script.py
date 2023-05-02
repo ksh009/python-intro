86a0d1e13c71fa86d57e4a0625d8c52be5e68eee
HughesSeanKyle <khughessean@yahoo.com>
2023-05-03 00:23:54 +0200

    Notes for Python solution

--- 2.reverse-string/script.py
+++ 2.reverse-string/script.py
@@ -2,6 +2,11 @@
 Reverse the provided string and return the reversed string.
 
         For example, "hello" should become "olleh".
+
+Notes: 
+    slice notation for a string is [start:end:step]
+
+    str[::-1], start is not specified, which means it defaults to the beginning of the string. end is also not specified, so it defaults to the end of the string. step is specified as -1, which means that the slice should step backwards through the string, effectively reversing it.
 """
 
 
