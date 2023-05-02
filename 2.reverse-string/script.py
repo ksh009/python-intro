6fa758bd8b5eaf7dcf2d5ac3dc8dca319225b9e1
HughesSeanKyle <khughessean@yahoo.com>
2023-05-03 00:15:19 +0200

    Python solution

---
+++ 2.reverse-string/script.py
@@ -0,0 +1,13 @@
+"""
+Reverse the provided string and return the reversed string.
+
+        For example, "hello" should become "olleh".
+"""
+
+
+def reverse_string(str):
+    return str[::-1]
+
+
+result = reverse_string("Hello")
+print(result)
